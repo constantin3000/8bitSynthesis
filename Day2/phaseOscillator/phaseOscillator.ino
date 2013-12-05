938bc8aa3d5bcfb46b2664c8ca2ac8b794c95052
Constantin Engelmann <constantin.e@gmx.de>
2013-12-05 22:12:46 +0100

    added function for fast PWM setup

--- Day2/phaseOscillator/phaseOscillator.ino
+++ Day2/phaseOscillator/phaseOscillator.ino
@@ -3,28 +3,45 @@ word phase_increment;
 word value;
 
 void setup(){
-  // set PWM Frequency on Pin 3/11 to 62,5kHz
-  #if defined (__AVR_ATmega168__) || defined (__AVR_ATmega48__) || defined (__AVR_ATmega88__) || defined (__AVR_ATmega328P__) || (__AVR_ATmega1280__)
-  // set Fast PWM Mode on Pin 3 and 11
-  TCCR2A = (TCCR2A & 0xfc) | 0x03;
-  // set prescaler to 1
-  TCCR2B = (TCCR2B & 0xf8) | 0x01;
-  #endif
+  setupPwmTimer();
   // our phase counter
   phase = 0;
   // has a useful resolution of 1..32768
   // to calculate phase increment: freq * max_phase/samplerate
   phase_increment = 3000; 
+  pinMode(10, OUTPUT);
+  // OUT2 on Esplora  
   pinMode(11, OUTPUT);
 }
 
 void loop(){
   digitalWrite(13, HIGH);
-  // increment phase by phase_increment
+  // increment phase by phase_increment  
   phase += phase_increment;
   // get the most significant 8 bits
   value = (phase>>8);
-  analogWrite(11, value);
+  analogWrite(10, value);
+//  analogWrite(11, value);
   digitalWrite(13, LOW);
   delayMicroseconds(100);
+}
+
+void setupPwmTimer(){
+  // Timer1 for our PWM output
+  // clear Timer1 Modes and Prescaler
+  TCCR1A = 0;
+  TCCR1B = 0;
+  // set prescaler of Timer1 to 1, => PWM Frequency on Pin 9/10 is 62500Hz (16MHz/256)
+  TCCR1B = (TCCR1B & 0xf8) | 0x01;  
+  // enable PWM on Pin 9 (COM1A1) and Pin 10 (COM1B1)
+  TCCR1A |= (1<<COM1A1) | (1<<COM1B1);
+
+  #if defined(COM1C1)
+  // for atmega32u4, Esplora OUT2
+  TCCR1A |= (1<<COM1C1);
+  #endif
+
+  // Fast PWM 8bit, see p. 138 atmega328 datasheet
+  TCCR1A |= (1<<WGM10);
+  TCCR1B |= (1<<WGM12);  
 }
