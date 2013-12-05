2d4d180a19be0d80335a2b597bcdaaec652c6788
Constantin Engelmann <constantin.e@gmx.de>
2013-12-05 23:36:35 +0100

    added ramp

--- Day2/Scripts/resources.h
+++ Day2/Scripts/resources.h
@@ -2,5 +2,6 @@
 
 extern const prog_uint8_t lut_sin[] PROGMEM;
 extern const prog_uint8_t lut_rsin[] PROGMEM;
+extern const prog_uint8_t lut_ramp[] PROGMEM;
 extern const prog_uint8_t lut_tri[] PROGMEM;
 extern const prog_uint16_t lut_pitch_increments[] PROGMEM;
